fce98d97434be47bcc9a901aca0e75d88df081ce
Anton Davydov <antondavydov.o@gmail.com>
2016-06-25 11:30:13 +0800

    Calls action generations

--- lib/hanami.rb
+++ lib/hanami.rb
@@ -2,19 +2,35 @@ require 'hanami'
 require 'hanami/scaffold'
 require 'hanami/scaffold/version'
 require 'hanami/cli_sub_commands/generate'
+require 'hanami/commands/generate/action'
 
 module Hanami
 end
 
 Hanami::CliSubCommands::Generate.define_commands do
+  ACTIONS = {
+    index:  'GET',
+    new:    'GET',
+    create: 'POST',
+    edit:   'GET',
+    update: 'PUT',
+    delete: 'DELETE'
+  }.freeze
+
   desc 'scaffold', 'Generate CRUD routes'
+  option :only, type: :array
+  option :expect, type: :array
   long_desc <<-EOS
   EOS
-  def scaffold
+  def scaffold(application_name = nil, controller_name)
     if options[:help]
       invoke :help, ['scaffold']
     else
-      puts 'scaffold generated'
+      ACTIONS.each do |action, method|
+        puts; say_status "Generate", "#{method} #{controller_name}##{action}"
+        Hanami::CliSubCommands::Generate.new.invoke :actions,
+          [application_name, "#{controller_name}##{action}"], method: method
+      end
     end
   end
 end
