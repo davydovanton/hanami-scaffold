2a55c1a566e03e62ed578b19cf9e1f0804ece611
Anton Davydov <antondavydov.o@gmail.com>
2017-07-22 12:56:41 +0300

    Update hanami CLI

--- lib/hanami/scaffold.rb
+++ lib/hanami/scaffold.rb
@@ -9,49 +9,45 @@ module Hanami
   end
 end
 
-Hanami::CliSubCommands::Generate.define_commands do
-  ACTIONS = {
-    'index'  => 'GET',
-    'new'    => 'GET',
-    'create' => 'POST',
-    'edit'   => 'GET',
-    'update' => 'PUT',
-    'delete' => 'DELETE'
-  }.freeze
+module Hanami
+  module Scaffold
+    module CLI
+      class Generate < Hanami::Cli::Commands::Command
+        ACTIONS = {
+          'index'  => 'GET',
+          'new'    => 'GET',
+          'create' => 'POST',
+          'edit'   => 'GET',
+          'update' => 'PUT',
+          'delete' => 'DELETE'
+        }.freeze
 
-  desc 'scaffold APPLICATION_NAME CONTROLLER_NAME', 'Generate CRUD routes'
-  option :only, type: :array
-  option :except, type: :array
-  long_desc <<-EOS
-    `hanami generate scaffold` generates an an actions, views and templates along with specs and a routes.
 
-    For Application architecture the application name is 'app'. For Container architecture the default application is called 'web'.
+        desc "Generate CRUD routes"
+        argument :application_name, default: :web
+        argument :project_name, required: true
+        option :only, type: :array
+        option :except, type: :array
+        def call(application_name, project_name, **options)
+          actions =
+            if options[:except] && options[:except].any?
+              ACTIONS.dup.delete_if { |key, _| options[:except].include?(key) }
+            elsif options[:only] && options[:only].any?
+              ACTIONS.dup.delete_if { |key, _| !options[:only].include?(key) }
+            else
+              ACTIONS
+            end
 
-    > $ hanami generate scaffold cars
-
-    > $ hanami generate scaffold web cars
-
-    > $ hanami generate scaffold web cars --only index new create show
-  EOS
-  def scaffold(application_name = :app, controller_name)
-    if options[:help]
-      invoke :help, ['scaffold']
-    else
-      actions =
-        if options[:except] && options[:except].any?
-          ACTIONS.dup.delete_if { |key, _| options[:except].include?(key) }
-        elsif options[:only] && options[:only].any?
-          ACTIONS.dup.delete_if { |key, _| !options[:only].include?(key) }
-        else
-          ACTIONS
+          actions.each do |action, method|
+            say_status "Generate", "#{method} #{controller_name}##{action}"
+            Hanami::CliSubCommands::Generate.new.invoke :actions,
+              [application_name, "#{controller_name}##{action}"], method: method
+            puts
+          end
         end
-
-      actions.each do |action, method|
-        say_status "Generate", "#{method} #{controller_name}##{action}"
-        Hanami::CliSubCommands::Generate.new.invoke :actions,
-          [application_name, "#{controller_name}##{action}"], method: method
-        puts
       end
     end
   end
 end
+
+Hanami::Cli.register "generate scaffold", Hanami::Scaffold::CLI::Generate
